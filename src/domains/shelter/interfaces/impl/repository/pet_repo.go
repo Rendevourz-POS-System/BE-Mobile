a03d890342630031bb7b4797b62452c5fae567cd
Reagent_Sandra <reagent.sandra@metrodata.co.id>
2024-07-04 22:31:01 +0700

    Shelter_id

--- src/domains/shelter/interfaces/impl/repository/pet_repo.go
+++ src/domains/shelter/interfaces/impl/repository/pet_repo.go
@@ -102,6 +102,11 @@ func (r *petRepo) filterPets(search *Pet.PetSearch) bson.D {
 			bson.M{"deleted_at": nil},
 		},
 	})
+	// Add filter to include only documents where ShelterId is not null
+	filter = append(filter, bson.E{
+		Key:   "shelter_id",
+		Value: bson.M{"$ne": nil},
+	})
 	return filter
 }
 
